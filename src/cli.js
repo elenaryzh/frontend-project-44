67b0ca7f3d2204c50abf65376d32e301da3c8d76
Elena Ryzhkovich <elenaryzhkovich@gmail.com>
2025-01-30 21:31:48 +0200

    Исправлена ошибка импорта функции welcomeUser

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+// src/cli.js
+import readlineSync from 'readline-sync';
+
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name? ');
+  
+  console.log(`Hello, ${name}!`);
+};
+
+export default welcomeUser;
